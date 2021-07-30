06e8d4863d5cf8ecc7bf51dc52641bb4ccb5dd73
padget <padget@users.noreply.github.com>
2021-07-30 20:12:04 +0200

    Update vector.test.cpp

--- cpp/test/vector.test.cpp
+++ cpp/test/vector.test.cpp
@@ -2,24 +2,39 @@
 #include <lib/vector.hpp>
 #include <lib/test.hpp>
 
-void should_initialize_empty_vector();
-void should_pushback_2_items();
-void should_reserve_1000_items();
+void should_initialize_empty_vector() 
+{
+  lib::vector<int> vec;
+  test_equals(vec.size(), 0);
+  test_equals(vec.capacity(), 10);
+  test_equals(vec.begin(), vec.end());
+}
+
+void should_pushback_2_items()
+{
+  lib::vector<int> vec;
+  vec.push_back(1);
+  vec.push_back(1);
+  test_equals(vec.size(), 2);
+  test_equals(vec.capacity(), 10);
+  test_equals(vec.begin() + 2, vec.end());
+}
+
+void should_reserve_1000_items()
+{
+  lib::vector<int> vec(1000);
+  test_equals(vec.size(), 0);
+  test_equals(vec.capacity(), 1000);
+  test_equals(vec.begin(), vec.end());
+}
+
 void should_copy_vector();
+
 void should_move_vector();
 
 int main()
 {
-  lib::vector<char> v(10);
-  v.push_back('0');
-  v.push_back('0');
-  v.push_back('0');
-  v.push_back('0');
-
-  const lib::vector<char> v2 = v;
-
-  for (const char &c : v2)
-  {
-    lib::printf("#", c);
-  }
+  should_initialize_empty_vector();
+  should_pushback_2_items();
+  should_reserve_1000_items();
 }
